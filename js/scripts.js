e97d759d83cb6df6411655f48f34d004c71b9f4f
Evan Clough <clough.evan@gmail.com>
2015-09-18 10:00:11 -0700

    Write spec for new size object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-function Constructor(thing){
-  this.thing = thing;
-  this.otherThing = [];
+function Pizza(size){
+  this.size = size;
+  this.toppings = [];
 };
-
-Thing.prototype.doThing = function() {
-  // do stuff
-};
-
-  $(document).ready(function() {
-    var thing = new Thing;
-
-    $("#elementid").click(function(event) {
-      $(this).text(text to change to);
-      thing.runSomeFunction();
-    });
-  });
+// 
+// Thing.prototype.doThing = function() {
+//   // do stuff
+// };
+//
+// $(document).ready(function() {
+//   var thing = new Thing;
+//
+//   $("#elementid").click(function(event) {
+//     $(this).text(text to change to);
+//     thing.runSomeFunction();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -6,11 +6,27 @@ describe('Pizza', function() {
     });
 });
 
+describe('Size', function() {
+    it("initiates initiates a new size", function() {
+        var medium = new Size("M");
+        expect(medium.name).to.equal("M")
+        expect(medium.basePrice).to.equal(16);
+        expect(medium.toppingsMultiplier).to.eql(1.3);
+    });
+});
+
+
+
+
+
 // object types:
 // pizza (size, toppings array)
-// topping (name)
+// topping (name, cost)
+// size (base cost, toppings multiplier)
 // order (pizza array)
 // user (name, order)
 //
 // prototype functions:
 // pizza.prototype.cost => calculate cost
+
+pizza has size and toppings. cost function
