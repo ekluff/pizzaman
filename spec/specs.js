624577ba4de34919dab15f3045491ee965b67fe0
Evan Clough <clough.evan@gmail.com>
2015-09-18 09:49:55 -0700

    Write spec to test initiation of pizza Object.

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,16 @@
-describe('thing', function() {
-    it("does stuff", function() {
-        var whatever = whatever;
-        expect(whatever).to.equal("whatever");
+describe('Pizza', function() {
+    it("initiates a new pizza", function() {
+        var testPizza = new Pizza("M");
+        expect(testPizza.size).to.equal("M");
+        expect(testPizza.toppings).to.eql([]);
     });
 });
+
+// object types:
+// pizza (size, toppings array)
+// topping (name)
+// order (pizza array)
+// user (name, order)
+//
+// prototype functions:
+// pizza.prototype.cost => calculate cost
